24543db26b39495e9f2d89b53d33cfffec21bd92
rikyiso01 <4943369@studenti.unige.it>
2024-12-05 13:06:37 +0100

    fix all

--- project/src/roofline-omp.sh
+++ project/src/roofline-omp.sh
@@ -3,6 +3,4 @@ source ./common.sh
 title "OMP parallel roofline"
 
 compile "$OMP_ROOFLINE" -g -xHost -O3 -ffast-math -DOMP
-roofline omp-roofline
-snapshot "omp-roofline"
-measure "omp-roofline"
+roofline roofline-omp-roofline
--- project/src/roofline.sh
+++ project/src/roofline.sh
@@ -3,15 +3,11 @@ source ./common.sh
 title Roofline
 
 compile "$ROOFLINE" -g
-roofline
-snapshot "hotspot"
-measure hotspot
+roofline "roofline-normal"
 
 compile "$ROOFLINE" -g -qopt-report=3 -xHost -O3
 mv ./mandelbrot.optrpt $OUT
 measure opt-report
 
-compile "$ROOFLINE" -g
-roofline
-snapshot "hotspot-best-sequential"
-measure hotspot-best-sequential
+compile "$ROOFLINE" -g -xHost -O3 -ffast-math
+roofline "roofline-optimized"
