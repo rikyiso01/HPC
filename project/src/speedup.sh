9439aa13bb160fe0fd552889a13f0f0e4ea53f63
Riccardo Isola <riky.isola@gmail.com>
2024-12-11 14:41:44 +0100

    all

--- project/src/speedup.sh
+++ project/src/speedup.sh
@@ -12,7 +12,7 @@ rm -rf "$OUT/speedup"
 for i in 1 2 4 12 20 30
 do
     compile "$SPEEDUP" -g -xHost -O3 -ffast-math -DOMP
-    echo "OMP_NUM_THREADS=$i" >> "$OUT/speedup"
+    echo "OMP_NUM_THREADS=$i" | tee -a "$OUT/speedup"
     time OMP_NUM_THREADS=$i $COMPILED_FILE "$PROGRAM_OUTPUT" | tee -a "$OUT/speedup"
 done
 
