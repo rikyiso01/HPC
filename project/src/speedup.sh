24ed5df5314c913646be79f53041634708f706d3
Riccardo Isola <riky.isola@gmail.com>
2024-12-11 14:40:44 +0100

    all

--- project/src/speedup.sh
+++ project/src/speedup.sh
@@ -5,3 +5,14 @@ title "OMP speedup"
 # compile "$SPEEDUP" -g -xHost -O3 -ffast-math -DOMP
 # roofline omp-roofline
 # measure "omp-roofline"
+
+
+rm -rf "$OUT/speedup"
+
+for i in 1 2 4 12 20 30
+do
+    compile "$SPEEDUP" -g -xHost -O3 -ffast-math -DOMP
+    echo "OMP_NUM_THREADS=$i" >> "$OUT/speedup"
+    time OMP_NUM_THREADS=$i $COMPILED_FILE "$PROGRAM_OUTPUT" | tee -a "$OUT/speedup"
+done
+
