09a0afd12d9759444457e01d7495472e0262f974
Gabriele Dellepere <gabriele.dellepere@nextage-on.com>
2024-12-20 09:44:55 +0100

    i had inverted source and dest buffer + minor fix

--- assignment-mpi/mpi_homework.c
+++ assignment-mpi/mpi_homework.c
@@ -4,11 +4,13 @@
                
 #define PI25DT 3.141592653589793238462643
 
+#ifndef INTERVALS
 #define INTERVALS 55000000000
+#endif
 
 int main(int argc, char **argv)
 {
-    long int i, intervals = INTERVALS;
+    long i, intervals = INTERVALS;
     double x, dx, f, sum, pi, result;
     double time2;
     
@@ -27,7 +29,7 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int work = intervals / size;
+    long work = intervals / size;
     int remainder = intervals % size;
 
     if (rank < remainder) { // then increase this process personal work by 1 and set its starting point to its rank * work_of_previous_processes
@@ -37,7 +39,7 @@ int main(int argc, char **argv)
         i = rank * work + remainder; // == (remainder * (work + 1)) + (rank - remainder) * work; 
     }
 
-    int limit = i + work;
+    long limit = i + work;
 
     for (; i < limit; i++) {
         // changed this to + 0.5 because it's simpler having i start from 0
@@ -46,7 +48,7 @@ int main(int argc, char **argv)
         sum = sum + f;
     }
 
-    MPI_Reduce(&result, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&sum, &result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
         pi = dx*result;
